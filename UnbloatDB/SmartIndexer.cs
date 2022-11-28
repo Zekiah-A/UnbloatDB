1e93893e066b5099220246be2ebf59676adf279b
Zekiah-A <zekiahamoako@outlook.com>
2022-11-28 00:27:35 +0000

    Fix case jumping

--- UnbloatDB/SmartIndexer.cs
+++ UnbloatDB/SmartIndexer.cs
@@ -63,21 +63,22 @@ internal sealed class SmartIndexer
 
             var values = index.Select(keyValue => keyValue[0]) as string[];
             var propertyValue = property.GetValue(record);
-                
-            // Figure out where to put in index, so we do not need to sort later by first binary searching for same value,
-            // and appending after, if not alr in the array, we analyse where it should go.
-            var foundIndex = Array.BinarySearch(values, propertyValue);
 
-            if (foundIndex != -1)
+            if (values is not null)
             {
-                index.Insert(foundIndex - 1,new[] { propertyValue as string, record.MasterKey });
-                continue;
+                // Figure out where to put in index, so we do not need to sort later by first binary searching for
+                // same value, and appending after, if not alr in the array, we analyse where it should go.
+                var foundIndex = Array.BinarySearch(values, propertyValue);
+
+                if (foundIndex != -1)
+                {
+                    index.Insert(foundIndex - 1, new[] {propertyValue as string, record.MasterKey});
+                }
             }
-
-            // If we could not binary search in the index for another key with the same value we can place this before,
-            // iterate through values until we find a value that is greater than new, and then jump back by one to give a sorted list.
-            if (values.Length > 0)
+            else if (values?.Length > 0)
             {
+                // If we could not binary search in the index for another key with the same value we can place this before,
+                // iterate through values until we find a value that is greater than new, and then jump back by one to give a sorted list.
                 for (var i = 0; i < values.Length; i++)
                 {
                     if (values[i].CompareTo(propertyValue) == -1)
@@ -87,12 +88,12 @@ internal sealed class SmartIndexer
                     
                     index.Insert(i - 1,new[] { propertyValue as string, record.MasterKey });
                 }
-                
-                continue;
             }
-            
-            // If no previous approaches worked (index length is probably zero/empty), then just add value to end of index.
-            index.Add(new[] { propertyValue as string, record.MasterKey });
+            else
+            {
+                // If no previous approaches worked (index length is probably zero/empty), then just add value to end of index.
+                index.Add(new[] { propertyValue as string, record.MasterKey });
+            }
             
             await File.WriteAllLinesAsync(indexPath, index.Select(pair => string.Join(" ", pair)));
         }
